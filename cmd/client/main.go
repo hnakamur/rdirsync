2fc26bcf2db064934b8807e975a41b5d8bf7a5d4
Hiroaki Nakamura <hnakamur@gmail.com>
2016-10-17 23:52:13 +0900

    Rewrite client with subcommands fetch and send

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/hnakamur/rdirsync"
 
@@ -11,41 +13,94 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
+var name = "client"
+
+const globalUsage = `Usage: %s <subcommand> [options] srcDirOrFile destDirOrFile
+
+This is an example client command to synchronize directories and files between
+the localhost and a remote server.
+
+subcommands:
+    send       send a local directory or file to the remote server.
+    fetch      fetch a remote directory or file to the localhost.
+
+`
+
 func main() {
-	var enableTLS bool
-	flag.BoolVar(&enableTLS, "enable-tls", false, "enable TLS")
-	var caFile string
-	flag.StringVar(&caFile, "ca-file", "../../ssl/ca/cacert.pem", "The file containning the CA root cert file")
-	var serverHostOverride string
-	flag.StringVar(&serverHostOverride, "server-host-override", "grpc.example.com", "The server name use to verify the hostname returned by TLS handshake")
-	var serverAddr string
-	flag.StringVar(&serverAddr, "server-addr", "127.0.0.1:10000", "server listen address")
-	var command string
-	flag.StringVar(&command, "command", "fetch", "operation: one of fetch, fetchdir, send, senddir")
-	var remotePath string
-	flag.StringVar(&remotePath, "remote-path", "/home/hnakamur/gocode/src/github.com/hnakamur/rdirsync/rpc/rdirsync.proto", "file path to fetch")
-	var localPath string
-	flag.StringVar(&localPath, "local-path", "rdirsync.proto", "file path to save")
-	var atMostCount int
-	flag.IntVar(&atMostCount, "at-most-count", 16, "at most file info count per rpc")
-	var keepDeletedFiles bool
-	flag.BoolVar(&keepDeletedFiles, "keep-deleted-files", false, "wether or not keep deleted files")
-	var syncModTime bool
-	flag.BoolVar(&syncModTime, "sync-mod-time", false, "sync modification time")
-	var updateOnly bool
-	flag.BoolVar(&updateOnly, "update-only", false, "skip update files whose size is the same and mod time is equal or newer")
+	os.Exit(run())
+}
+
+func run() int {
+	flag.Usage = func() {
+		fmt.Printf(globalUsage, name)
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.Usage()
+		return 1
+	}
+
+	switch args[0] {
+	case "send":
+		return handleSendCommand(args[1:])
+	case "fetch":
+		return handleFetchCommand(args[1:])
+	default:
+		flag.Usage()
+		return 1
+	}
+}
+
+type options struct {
+	// grpc dial options
+	serverAddr         string
+	serverHostOverride string
+	enableTLS          bool
+	caFile             string
+
+	// rdirsync options
+	bufSize                 int
+	maxEntriesPerReadDirRPC int
+	keepDeletedFiles        bool
+	syncModTime             bool
+	updateOnly              bool
+}
+
+func parseOptions(subcommand, usage string, args []string) (*flag.FlagSet, *options) {
+	fs := flag.NewFlagSet(subcommand, flag.ExitOnError)
+	fs.Usage = func() {
+		fmt.Printf(usage, name)
+		fs.PrintDefaults()
+	}
+
+	var opts options
+	fs.StringVar(&opts.serverAddr, "server-addr", "127.0.0.1:10000", "server address to connect")
+	fs.StringVar(&opts.serverHostOverride, "server-host-override", "grpc.example.com", "The server name use to verify the hostname returned by TLS handshake")
+	fs.BoolVar(&opts.enableTLS, "enable-tls", false, "enable TLS")
+	fs.StringVar(&opts.caFile, "ca-file", "cacert.pem", "The file containning the CA root cert file")
+	fs.IntVar(&opts.bufSize, "buf-size", 64*1024, "buffer size for reading a file")
+	fs.IntVar(&opts.maxEntriesPerReadDirRPC, "at-most-count", 1024, "at most file info count per readdir rpc")
+	fs.BoolVar(&opts.keepDeletedFiles, "keep-deleted-files", false, "wether or not keep deleted files")
+	fs.BoolVar(&opts.syncModTime, "sync-mod-time", false, "sync modification time")
+	fs.BoolVar(&opts.updateOnly, "update-only", false, "skip update files whose size is the same and mod time is equal or newer")
+	fs.Parse(args)
+	return fs, &opts
+}
+
+func (o *options) buildGrpcOptions() []grpc.DialOption {
 	var opts []grpc.DialOption
-	if enableTLS {
+	if o.enableTLS {
 		var sn string
-		if serverHostOverride != "" {
-			sn = serverHostOverride
+		if o.serverHostOverride != "" {
+			sn = o.serverHostOverride
 		}
 		var creds credentials.TransportCredentials
-		if caFile != "" {
+		if o.caFile != "" {
 			var err error
-			creds, err = credentials.NewClientTLSFromFile(caFile, sn)
+			creds, err = credentials.NewClientTLSFromFile(o.caFile, sn)
 			if err != nil {
 				log.Fatalf("Failed to create TLS credentials %v", err)
 			}
@@ -56,44 +111,91 @@ func main() {
 	} else {
 		opts = append(opts, grpc.WithInsecure())
 	}
+	return opts
+}
 
-	conn, err := grpc.Dial(serverAddr, opts...)
+func (o *options) buildRDirSyncClientOptions() []rdirsync.ClientOptionFunc {
+	return []rdirsync.ClientOptionFunc{
+		rdirsync.SetBufSize(o.bufSize),
+		rdirsync.SetMaxEntriesPerReadDirRPC(o.maxEntriesPerReadDirRPC),
+		rdirsync.SetKeepDeletedFiles(o.keepDeletedFiles),
+		rdirsync.SetSyncModTime(o.syncModTime),
+		rdirsync.SetUpdateOnly(o.updateOnly),
+	}
+}
+
+const fetchUsage = `Usage: %s fetch [options] remoteDestDirOrFile localSrcDirOrFile
+
+options:
+`
+
+func handleFetchCommand(args []string) int {
+	fs, opts := parseOptions("fetch", fetchUsage, args)
+	if fs.NArg() != 2 {
+		fs.Usage()
+		return 1
+	}
+
+	remotePath := fs.Arg(0)
+	localPath := fs.Arg(1)
+
+	conn, err := grpc.Dial(opts.serverAddr, opts.buildGrpcOptions()...)
 	if err != nil {
-		log.Fatalf("fail to dial: %+v", err)
+		fmt.Printf("fail to connect to server: %+v", err)
+		return 1
 	}
 	defer conn.Close()
-	client, err := rdirsync.NewClient(conn,
-		rdirsync.SetMaxEntriesPerReadDirRPC(atMostCount),
-		rdirsync.SetKeepDeletedFiles(keepDeletedFiles),
-		rdirsync.SetSyncModTime(syncModTime),
-		rdirsync.SetUpdateOnly(updateOnly),
-	)
+
+	client, err := rdirsync.NewClient(conn, opts.buildRDirSyncClientOptions()...)
 	if err != nil {
-		log.Fatalf("fail to create client: %+v", err)
+		fmt.Printf("fail to create client: %+v", err)
+		return 1
 	}
+
 	ctx := context.Background()
-	switch command {
-	case "fetch":
-		err := client.FetchFile(ctx, remotePath, localPath)
-		if err != nil {
-			log.Fatalf("failed to fetch file; %s", err)
-		}
-	case "fetchdir":
-		err := client.FetchDir(ctx, remotePath, localPath)
-		if err != nil {
-			log.Fatalf("failed to fetch directory; %s", err)
-		}
-	case "send":
-		err := client.SendFile(ctx, localPath, remotePath)
-		if err != nil {
-			log.Fatalf("failed to send file; %s", err)
-		}
-	case "senddir":
-		err := client.SendDir(ctx, localPath, remotePath)
-		if err != nil {
-			log.Fatalf("failed to send directory; %s", err)
-		}
-	default:
-		log.Fatalf("Unsupported command: %s", command)
+	err = client.Fetch(ctx, remotePath, localPath)
+	if err != nil {
+		fmt.Printf("failed to fetch directory of file; %s", err)
+		return 1
 	}
+
+	return 0
+}
+
+const sendUsage = `Usage: %s send [options] localSrcDirOrFile remoteDestDirOrFile
+
+options:
+`
+
+func handleSendCommand(args []string) int {
+	fs, opts := parseOptions("send", sendUsage, args)
+	if fs.NArg() != 2 {
+		fs.Usage()
+		return 1
+	}
+
+	localPath := fs.Arg(0)
+	remotePath := fs.Arg(1)
+
+	conn, err := grpc.Dial(opts.serverAddr, opts.buildGrpcOptions()...)
+	if err != nil {
+		fmt.Printf("fail to connect to server: %+v", err)
+		return 1
+	}
+	defer conn.Close()
+
+	client, err := rdirsync.NewClient(conn, opts.buildRDirSyncClientOptions()...)
+	if err != nil {
+		fmt.Printf("fail to create client: %+v", err)
+		return 1
+	}
+
+	ctx := context.Background()
+	err = client.Send(ctx, localPath, remotePath)
+	if err != nil {
+		fmt.Printf("failed to fetch directory of file; %s", err)
+		return 1
+	}
+
+	return 0
 }
